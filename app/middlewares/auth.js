ffa633c3ee09bf9967d251014bfa793686cfa829
Seth Zhang <seth.zhang@cyaninc.com>
2014-01-11 00:44:08 -0800

    working

--- app/middlewares/auth.js
+++ app/middlewares/auth.js
@@ -19,60 +19,55 @@
 var __ = require('underscore');
 
 function userNotAuthenticated(req, res, e) {
-  req.session.user = null;
-  if (req.url.search(/^\/api\/v\d*\/[a-zA-Z0-9\/\-%]*/) !== -1) {
-    res.json(401, {
-      'result': 'false',
-      'message': !e ? 'User not authenticated' : e,
-      'description': 'Invalid Cookie. Please login',
-      'source': 'youyun server'
-    });
-  } else {
-    // redirect to login page
-    res.redirect('/login');
-  }
+	req.session.user = null;
+	if (req.url.search(/^\/api\/v\d*\/[a-zA-Z0-9\/\-%]*/) !== -1) {
+		res.json(401, {
+			'result': 'false',
+			'message': !e ? 'User not authenticated' : e,
+			'description': 'Invalid Cookie. Please login',
+			'source': 'youyun server'
+		});
+	} else {
+		// redirect to login page
+		res.redirect('/login');
+	}
 }
 
 function isUserAuthenticated(req, res, next) {
-  // tmp workaround
-  // req.session.user.api_key = 1;
-  /* jshint -W106 */
-  console.log(JSON.stringify(req.session));
-  if (!req.session.passport.user ||
-      !req.session.cookie) {
-    userNotAuthenticated(req, res);
-  } else {
-    next();
-  }
-  /* jshint +W106 */
+	// tmp workaround
+	// req.session.user.api_key = 1;
+	/* jshint -W106 */
+	console.log(JSON.stringify(req.session));
+	if (!req.session.passport.user ||
+			!req.session.cookie) {
+		userNotAuthenticated(req, res);
+	} else {
+		next();
+	}
+	/* jshint +W106 */
 }
 
 module.exports.checkUserSession = function(req, res, next) {
-  var path = req.url;
-  var isInWhitelist = false;
-  var whitelistPatterns = [
-    /^\/(login|logout)/,
-    /^\/api\/v\d\/accounts\/(login|logout|getuser)/
-  ];
+	var path = req.url;
+	var isInWhitelist = false;
+	var whitelistPatterns = [
+		/^\/(login|logout)/,
+		/^\/api\/v\d\/accounts\/(login|logout|getuser)/,
+		/^\/static\//
+	];
 
+	if (path === '/') {
+		isInWhitelist = true;
+	} else {
+		__.each(whitelistPatterns, function(pattern) {
+			isInWhitelist = isInWhitelist || (path.search(pattern) !== -1);
+		});
+	}
 
-  // TODO change?
-  if (process.env.NODE_ENV !== 'nginx') {
-    whitelistPatterns.push(/^\/static\//);
-  }
-
-  if (path === '/') {
-    isInWhitelist = true;
-  } else {
-    __.each(whitelistPatterns, function(pattern) {
-      isInWhitelist = isInWhitelist || (path.search(pattern) !== -1);
-    });
-  }
-
-  if (isInWhitelist) {
-    next();
-  } else {
-    isUserAuthenticated(req, res, next);
-  }
+	if (isInWhitelist) {
+		next();
+	} else {
+		isUserAuthenticated(req, res, next);
+	}
 };
 
