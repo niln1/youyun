f811c9b81955f913b1fb3fb54f97b04f9f1a2808
Seth Zhang <seth.zhang@cyaninc.com>
2014-01-17 02:41:22 -0800

    exec worked

--- Gruntfile.js
+++ Gruntfile.js
@@ -115,6 +115,13 @@ module.exports = function(grunt) {
                 },
                 command: 'scripts/r.js.sh'
             }
+        },
+        exec: {
+            js: {
+                command: 'grep -nr "data\-main" server/views | while read line; do file=`echo "$line" | sed "s|^.*data-main\s*=\s*||" | tr -d "()\"\\\'"" | sed "s|^\\\/||; s|static\\\/js|.tmp-build\\\/ts|"`; if [[ "$file" != *.min ]]; then outfile=`echo "$file.js" | sed "s|^.tmp-build\/ts|production\/js|;s|\.js|\.min\.js|"`; r.js -o baseUrl=`dirname "$file"` name=`basename "$file"` out="$outfile";  echo "requirejs(["`basename "$file"`"]);" >> "$outfile"; fi; done',
+                stdout: true,
+                stderr: true
+            }
         }
     });
 
