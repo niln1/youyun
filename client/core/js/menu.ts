6b714debd66a225aaf8a415de907ac96b689a87a
Ranchao Zhang <r.c.cham@gmail.com>
2014-02-06 22:27:18 -0800

    [client]moved header to a typescript class

--- client/core/js/menu.ts
+++ client/core/js/menu.ts
@@ -3,10 +3,16 @@
 /// <amd-dependency path="templates/menu" />
 
 class Menu {
+    public static MAIN_MENU_SELECTOR : string = '.main-menu';
+
     constructor () {
         // TODO JQuery selector
         var menuTemplate = require('templates/menu');
-        console.log('Here');
+        if (menuTemplate && menuTemplate.template) {
+            var menuContainer = $(Menu.MAIN_MENU_SELECTOR);
+            var menuTemplate = menuTemplate.template({});
+            menuContainer.append(menuTemplate);
+        }
     }
 
     public showMenu() {
