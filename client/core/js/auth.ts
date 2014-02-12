1b2d85c57ee3448bdd3681377b59ea5bf129eb71
Ranchao Zhang <r.c.cham@gmail.com>
2014-02-12 08:57:51 -0800

    [client]need to add DeepModel

--- client/core/js/auth.ts
+++ client/core/js/auth.ts
@@ -71,7 +71,7 @@ class Auth {
     }
 
     public logout () {
-        
+
     }
 
     private save() : void {
--- client/core/js/user.ts
+++ client/core/js/user.ts
@@ -1,17 +1,28 @@
 
 /// <reference path='interfaces/db.d.ts'/>
+/// <reference path='vendor/backbone/marionette.d.ts'/>
 
-class User {
+class User extends Backbone.DeepModel {
     private __v : string;
     private _id : string;
     private username : string;
 //    TODO: add class Class
 //    private classes : Class[];
 
-    constructor(json ?: IUserSchema) {
-        this.__v = json.__v;
-        this._id = json._id;
-        this.username = json.username;
+    public _validators:{[index:string]:string[]};
+    private static validators:{[index:string]:string[]} = {
+        __V: ['required'],
+        _id: ['required'],
+        username: ['required']
+    };
+
+    constructor(attrs?:any, options?:any) {
+        super(attrs, options);
+
+        this._validators = User.validators;
+//        this.__v = json.__v;
+//        this._id = json._id;
+//        this.username = json.username;
     }
 }
 
