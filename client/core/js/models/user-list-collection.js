489045bf421816933bf60d0e933e049ae665a52a
Ni Zhihao <niz@rose-hulman.edu>
2014-04-19 22:16:47 -0700

    [client]user model added

---
+++ client/core/js/models/user-list-collection.js
@@ -0,0 +1,20 @@
+define(["require", "exports", './user-model'], function (require, exports, UserModel) {
+    var UserListCollection = (function (_super) {
+        __extends(UserListCollection, _super);
+
+        function UserListCollection(models, options) {
+            this.url = '/api/v1/users?signature=tempkey';
+            _super.call(this, models, {
+                model: UserModel
+            });
+        }
+        UserListCollection.prototype.parse = function (response) {
+            console.log(response);
+            return response.result;
+        };
+
+        return UserListCollection;
+    })(Backbone.Collection);
+
+    return UserListCollection;
+});
