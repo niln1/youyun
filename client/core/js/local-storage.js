503aa7a58296b6d8580c6c66291559f0f0b9fb3a
Ni Zhihao <niz@rose-hulman.edu>
2014-04-19 22:57:57 -0700

    [client]contact view

--- client/core/js/local-storage.js
+++ client/core/js/local-storage.js
@@ -1,51 +1,54 @@
-define(["require", "exports"], function (require, exports) {
-    var LocalStorage = (function () {
-        function LocalStorage() {
-            this.settings = amplify.store(LocalStorage.kKEY) || {};
-        }
-        Object.defineProperty(LocalStorage, "I", {
-            get: function () {
-                return LocalStorage.instance || (LocalStorage.instance = new LocalStorage());
-            },
-            enumerable: true,
-            configurable: true
-        });
-
-        LocalStorage.prototype.get = function (valueKey, defaultValue) {
-            if (this.settings[valueKey]) {
-                return this.settings[valueKey];
+define(["require",
+    "exports"],
+    function (require, exports) {
+        var LocalStorage = (function () {
+            function LocalStorage() {
+                this.settings = amplify.store(LocalStorage.kKEY) || {};
             }
+            Object.defineProperty(LocalStorage, "I", {
+                get: function () {
+                    return LocalStorage.instance || (LocalStorage.instance = new LocalStorage());
+                },
+                enumerable: true,
+                configurable: true
+            });
 
-            if (defaultValue) {
-                this.settings[valueKey] = defaultValue;
+            LocalStorage.prototype.get = function (valueKey, defaultValue) {
+                if (this.settings[valueKey]) {
+                    return this.settings[valueKey];
+                }
+
+                if (defaultValue) {
+                    this.settings[valueKey] = defaultValue;
+
+                    this.save();
+
+                    return defaultValue;
+                } else {
+                    return null;
+                }
+            };
+
+            LocalStorage.prototype.set = function (valueKey, value) {
+                this.settings[valueKey] = value;
 
                 this.save();
+            };
 
-                return defaultValue;
-            } else {
-                return null;
-            }
-        };
+            LocalStorage.prototype.remove = function (valueKey) {
+                delete this.settings[valueKey];
 
-        LocalStorage.prototype.set = function (valueKey, value) {
-            this.settings[valueKey] = value;
+                this.save();
+            };
 
-            this.save();
-        };
+            LocalStorage.prototype.save = function () {
+                amplify.store(LocalStorage.kKEY, this.settings);
+            };
+            LocalStorage.kKEY = "local-storage";
+            return LocalStorage;
+        })();
 
-        LocalStorage.prototype.remove = function (valueKey) {
-            delete this.settings[valueKey];
 
-            this.save();
-        };
-
-        LocalStorage.prototype.save = function () {
-            amplify.store(LocalStorage.kKEY, this.settings);
-        };
-        LocalStorage.kKEY = "local-storage";
         return LocalStorage;
-    })();
-
-
-    return LocalStorage;
-});
+    }
+);
