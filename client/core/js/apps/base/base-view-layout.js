e4d02d87e2de8809cbd6b84e5c8262a01e82f636
Ni Zhihao <niz@rose-hulman.edu>
2014-04-20 20:20:12 -0700

    [client]refactering

--- client/core/js/apps/base/base-view-layout.js
+++ client/core/js/apps/base/base-view-layout.js
@@ -1,20 +1,47 @@
-define(["require", "exports", '../../config/base-layout', './base-view-controller', "./templates/base-view-layout-tmpl"], function (require, exports, BaseLayout, BaseViewController) {
-    var BaseViewLayout = (function (_super) {
-        __extends(BaseViewLayout, _super);
+define(["require",
+    "exports",
+    '../../config/base-layout',
+    '../header/header-view',
+    '../header-background/header-background-view',
+    '../main/main-view-layout'
+    "./templates/base-view-layout-tmpl"],
+    function (require, exports, BaseLayout, HeaderView, HeaderBackgroundView, MainViewLayout, BaseViewController) {
+        var BaseViewLayout = (function (_super) {
+            __extends(BaseViewLayout, _super);
 
-        function BaseViewLayout() {
-            _super.call(this);
-            this.template = require('./templates/base-view-layout-tmpl');
-            this.addRegions({
-                headerRegion: '#header-region',
-                headerBackgroundRegion: '#header-background-region',
-                mainRegion: '#main-region'
-            });
-            this.controller = new BaseViewController(this);
-        }
+            function BaseViewLayout() {
+                _super.call(this);
+                this.template = require('./templates/base-view-layout-tmpl');
+                this.addRegions({
+                    headerRegion: '#header-region',
+                    headerBackgroundRegion: '#header-background-region',
+                    mainRegion: '#main-region'
+                });
+            }
+
+            BaseViewLayout.prototype.initSubViews = function () {
+                this.showHeaderView();
+                this.showHeaderBackgroundView();
+                this.showMainViewLayout();
+            };
+            BaseViewLayout.prototype.showHeaderView = function () {
+                this.headerRegion.show(new HeaderView());
+            };
+            BaseViewLayout.prototype.showHeaderBackgroundView = function () {
+                var headerBackgroundView = new HeaderBackgroundView();
+                this.headerBackgroundRegion.show(headerBackgroundView);
+                headerBackgroundView.controller.collapse();
+            };
+            BaseViewLayout.prototype.showMainViewLayout = function () {
+                var mainViewLayout = new MainViewLayout();
+                this.mainRegion.show(mainViewLayout);
+                mainViewLayout.showFeedCenter();
+                mainViewLayout.showLeftPanel();
+            };
+            return BaseViewLayout;
+        })(BaseLayout);
+
+
         return BaseViewLayout;
-    })(BaseLayout);
-
-
-    return BaseViewLayout;
-});
+    }
+);
