108b1df366b52c3e33fb15e8c1c923fa39a2722e
Ni Zhihao <niz@rose-hulman.edu>
2014-04-21 22:15:51 -0700

    [client]progress

--- client/core/js/models/user-list-collection.js
+++ client/core/js/models/user-list-collection.js
@@ -1,20 +1,24 @@
-define(["require", "exports", './user-model'], function (require, exports, UserModel) {
-    var UserListCollection = (function (_super) {
-        __extends(UserListCollection, _super);
+define(["require",
+    "exports",
+    './user-model'],
+    function (require, exports, UserModel) {
+        var UserListCollection = (function (_super) {
+            __extends(UserListCollection, _super);
 
-        function UserListCollection(models, options) {
-            this.url = '/api/v1/users?signature=tempkey';
-            _super.call(this, models, {
-                model: UserModel
-            });
-        }
-        UserListCollection.prototype.parse = function (response) {
-            console.log(response);
-            return response.result;
-        };
+            function UserListCollection(models, options) {
+                this.url = '/api/v1/users?signature=tempkey';
+                _super.call(this, models, {
+                    model: UserModel
+                });
+            }
+            UserListCollection.prototype.parse = function (response) {
+                console.log(response);
+                return response.result;
+            };
+
+            return UserListCollection;
+        })(Backbone.Collection);
 
         return UserListCollection;
-    })(Backbone.Collection);
-
-    return UserListCollection;
-});
+    }
+);
