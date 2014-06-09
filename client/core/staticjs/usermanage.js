780d23eddc0a491c5d6f623f63a284a35aa513f1
Ni Zhihao <niz@rose-hulman.edu>
2014-06-08 23:36:56 -0700

    new stuff

--- client/core/staticjs/usermanage.js
+++ client/core/staticjs/usermanage.js
@@ -1,1 +1,24 @@
-var x = 1;
+/*
+ * file: usermanage.js
+ * Copyright (c) 2014, Zhihao Ni & Ranchao Zhang. All rights reserved.
+ */
+
+"use strict";
+
+var userManageApp = (function () {
+    function View() {
+        this.$studentTable = $("#student-table");
+    }
+    View.prototype.start = function () {
+        console.log(this.$studentTable);
+    };
+    View.prototype.getUserList = function () {
+
+    };
+    return View;
+})();
+
+$(function () {
+    var app = new userManageApp();
+    app.start();
+});
