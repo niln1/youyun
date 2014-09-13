fc1ca58b01bfcd46d27f175e0077d3b1dde8a08d
Ni Zhihao <niz@rose-hulman.edu>
2014-09-13 10:29:37 -0700

    progress

--- utilities/populate_db_by_csv.js
+++ utilities/populate_db_by_csv.js
@@ -20,18 +20,19 @@ var User = require('../server/models/User');
 mongoose.connect(config['mongodb-url']);
 
 var db = mongoose.connection;
-db.on('error', function() {
-  console.log('mongod connection error', config['mongodb-url']);
+db.on('error', function(err) {
+  console.log("mongoErr:", err);
 });
 
 var helper = (function(){
   function App() {
     console.log("hello");
+    this.students = [];
+    this.parents = [];
   }
 
   App.prototype.parseStudentCsv = function() {
     var self = this;
-    var output = [];
     var studentCsvStream = fs.createReadStream(__dirname + '/datasource/student.csv');
 
     var parser = csv.parse({
@@ -40,23 +41,77 @@ var helper = (function(){
       skip_empty_lines:true,
       columns: true
     }, function(err, datas){
-      output.push(datas);
       __.each(datas, function(data) {
-          self.generateUsernameByFnLn(data.firstname, data.lastname, 0)
-          .then(function (name) {
-            console.log(name);
-          })
-          .fail(function (err) {
-            console.log("ERROR", err);
-          });
-      })
+         self.createStudent(data);
+      });
     });
 
     studentCsvStream.pipe(parser)
     .on("end", function(){
-      console.log("done");
+      console.log("done parsing student");
     });
-  }
+  };
+
+  App.prototype.parseStudentParentCsv = function() {
+    var self = this;
+    var studentCsvStream = fs.createReadStream(__dirname + '/datasource/studentparent.csv');
+
+    var parser = csv.parse({
+      ltrim:true,
+      rtrim:true,
+      skip_empty_lines:true,
+      columns: true
+    }, function(err, datas){
+      var parents = __.pluck(datas, "parentsname");
+      console.log(parents);
+      __.each(parents, function(name) {
+         self.createParent(name);
+      });
+    });
+
+    studentCsvStream.pipe(parser)
+    .on("end", function(){
+      console.log("done parsing student parent");
+    });
+  };
+
+  App.prototype.createStudent = function (data) {
+    var self = this;
+
+    this.generateUsernameByFnLn(data.firstname, data.lastname, 0)
+    .then(function (name) {
+      tempPassword = self.generateRandomPassword(10);
+      var user = { username: name, password: tempPassword };
+      console.log("Username:", name, "==","Password", tempPassword, "Name:"+data.firstname + " " + data.lastname );
+      self.students.push(user);
+    })
+    .fail(function (err) {
+      console.log("ERROR", err);
+    });
+  };
+
+  App.prototype.createParent = function (name) {
+    var self = this;
+
+    this.generateUsernameByFnLn(name, "", 0)
+    .then(function (uname) {
+      tempPassword = self.generateRandomPassword(10);
+      var user = { username: uname, password: tempPassword };
+      console.log("Parent// Username:", uname, "==","Password", tempPassword, "Name:"+ name + " " + " " );
+      self.parents.push(user);
+    })
+    .fail(function (err) {
+      console.log("ERROR", err);
+    });
+  };
+
+  App.prototype.createTeacher = function (data) {
+
+  };
+
+  App.prototype.createAdmin = function (data) {
+
+  };
 
   App.prototype.generateUsernameByFnLn = function( firstname, lastname, hit ) {
     var self = this;
@@ -79,10 +134,35 @@ var helper = (function(){
     });
 
     return deferred.promise;
-  }
+  };
+
+  App.prototype.generateRandomPassword = function ( plength ) {
+    var keylistalpha="abcdefghijklmnopqrstuvwxyz";
+    var keylistint="123456789";
+    var keylistspec="!@#_";
+    var temp='';
+    var len = plength/2;
+    var len = len - 1;
+    var lenspec = plength-len-len;
+
+    for (i=0;i<len;i++)
+        temp+=keylistalpha.charAt(Math.floor(Math.random()*keylistalpha.length));
+
+    for (i=0;i<lenspec;i++)
+        temp+=keylistspec.charAt(Math.floor(Math.random()*keylistspec.length));
+
+    for (i=0;i<len;i++)
+        temp+=keylistint.charAt(Math.floor(Math.random()*keylistint.length));
+
+        temp=temp.split('').sort(function(){return 0.5-Math.random()}).join('');
+
+    return temp;
+  };
 
   return App;
+
 })();
 
 var app = new helper();
 app.parseStudentCsv();
+app.parseStudentParentCsv();
