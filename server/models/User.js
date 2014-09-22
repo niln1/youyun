8885cbeaff2ef2686d2450e630d3487f33d4fdec
Tracy Hu <jiamin.hu.tracy@gmail.com>
2014-09-21 20:03:36 -0700

    pre/post validate user???

--- server/models/User.js
+++ server/models/User.js
@@ -97,6 +97,22 @@ UserSchema.pre('save', function (next) {
 
 });
 
+UserSchema.pre('validate', function (next) {
+    console.log("hi:" , this);
+    this.password = "Black Sheep Wall";
+
+    console.log("hi again", this);
+    next();
+});
+
+
+UserSchema.post('validate', function (doc) {
+    console.log("hi:" , this);
+    doc.password = "Black Sheep Wall";
+
+    console.log("doc  ", this);
+});
+
 UserSchema.methods.comparePassword = function (candidatePassword, cb) {
     bcrypt.compare(candidatePassword, this.password, function (err, isMatch) {
         if (err) return cb(err);
