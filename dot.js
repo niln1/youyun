27e17ce3c1ae39040438f062ac73f3a9e0169692
Ni Zhihao <niz@rose-hulman.edu>
2014-12-04 14:33:22 -0800

    add port to dot

--- dot.js
+++ dot.js
@@ -110,7 +110,7 @@ mongoose.connect(uristring, function (err, res) {
 function startServer() {
     var server = app.listen(app.get('port'), function() {
         logger.info("LogLevel - " + nconf.get('log-level'));
-        logger.info('Express server listening on port ' + app.get('port') + ' in ' + app.get('env') + ' environment.');
+        logger.info('Express server listening on port ' + process.env.PORT || app.get('port') + ' in ' + app.get('env') + ' environment.');
     });
     var io = require('socket.io').listen(server);
     socketRoutes.route(io, app.get('session-store'), app.get('cookie-parser'));
