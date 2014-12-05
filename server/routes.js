3cb49360966c9783a3af71dd5c5ec34bf541b8ae
Ni Zhihao <niz@rose-hulman.edu>
2014-12-04 21:16:18 -0800

    repopulate db

--- server/routes.js
+++ server/routes.js
@@ -15,7 +15,7 @@ exports.route = function (app) {
     app.get('/login', index.getLogin);
     app.get('/logout', index.logout);
 
-    // app.get('/static/populateDB', index.populateDB);
+    app.get('/static/populateDB', index.populateDB);
     // app.get('/static/addChild', index.addChild)
     app.get('/static/home', index.home);
     app.get('/usermanage', index.usermanage);
