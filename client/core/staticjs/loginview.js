365b8e5e6577b565c89916e360de514808fdb517
Ni Zhihao <niz@rose-hulman.edu>
2015-06-27 22:34:36 -0700

    add modal to logon

--- client/core/staticjs/loginview.js
+++ client/core/staticjs/loginview.js
@@ -1,7 +1,17 @@
 $('#login-form input').on("focus", function highlightLogo () {
-	$('.school-logo').addClass('active');
+    $('.school-logo').addClass('active');
 });
 
 $('#login-form input').on("blur", function highlightLogo () {
-	$('.school-logo').removeClass('active');
+    $('.school-logo').removeClass('active');
 });
+
+$('.sign-up-btn').click(function () {
+
+})
+
+var App = {};
+
+App.showSignupPage = function () {
+    
+}
